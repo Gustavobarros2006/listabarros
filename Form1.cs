ace6bc9d4b996e2a6e6bc1e1eb9d9d8b502adb8e
joaoCanonica <jcanonicaescola@gmail.com>
2025-10-23 13:53:38 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listabarros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                // Adiciona o texto da TextBox à ListBox
+                listBox1.Items.Add(textBox1.Text);
+
+                // Opcional: Limpa a TextBox após adicionar o item
+                textBox1.Text = "";
+
+                // Opcional: Coloca o foco de volta na TextBox para facilitar a adição de múltiplos itens
+                textBox1.Focus();
+            }
+            else
+            {
+                // Opcional: Exibe uma mensagem se o usuário tentar adicionar um item vazio
+                MessageBox.Show("Por favor, digite um texto para adicionar.");
+            }
+        }
+    }
+}
